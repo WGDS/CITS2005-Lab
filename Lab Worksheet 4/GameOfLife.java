13e9c37d5671e415abb0d3b738e362cd9ddb1d30
Fubin QIU <1943960569@qq.com>
2025-04-02 21:30:16 +0800

    finish GameOfLife

--- Lab Worksheet 4/GameOfLife.java
+++ Lab Worksheet 4/GameOfLife.java
@@ -16,12 +16,38 @@ public class GameOfLife {
                     System.out.print(".");
                 }
             }
-            System.out.print("\n");
+            System.out.println();
         }
     }
 
     public void simulateStep() {
         // TODO
+        int[] row_offset = {-1,-1,-1,0,0,1,1,1};
+        int[] column_offset = {-1,0,1,-1,1,-1,0,1};
+        boolean[][] gridnext = new boolean[grid.length][grid[0].length];
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[i].length; j ++){
+                int liveCount = 0;
+                for(int k = 0; k<8; k++){
+                    int newRow = (i + row_offset[k] + grid.length) % grid.length;
+                    int newCol = (j + column_offset[k] + grid.length) % grid.length;
+                    if(grid[newRow][newCol]){
+                        liveCount ++;
+                    }
+                }
+                if((liveCount == 2 || liveCount == 3)&& grid[i][j] == true){
+                    gridnext[i][j] = true;
+                }
+                else if(liveCount == 3 && grid[i][j] == false){
+                    gridnext[i][j] = true;
+                }
+                else{
+                    gridnext[i][j] = false;
+                }
+
+            }
+        }
+        grid = gridnext;
     }
 
     public static void main(String[] args) {
